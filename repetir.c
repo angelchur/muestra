1051b93ee7284e2d21bc1c5146ce55c841e204f3
unknown <miguelchura65@gmail.com>
2024-10-23 13:37:45 -0300

    repetir simbolo

---
+++ repetir.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void linea(char simbolo) {
+    for(int i = 0; i < 20; i++) {
+        printf("%c", simbolo);
+    }
+    printf("\n");
+}
+
+int main() {
+linea ("#");
+    return 0;
+}
